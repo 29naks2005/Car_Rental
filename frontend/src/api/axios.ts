3e9c98bc5e3a5e67152dc9b1da566d522a6f7af2
29naks2005 <nakulsharma093@gmail.com>
2026-04-22 23:25:42 +0530

    cros

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://car-rental-1kjz.onrender.com/api",
 });
 
 // Attach JWT token to every request
